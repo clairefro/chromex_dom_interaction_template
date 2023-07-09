5f564493b5d1448c75c8b093e994c8d0182a8d80
Claire Froelich <claire.froelich@gmail.com>
2023-07-09 15:03:07 -0400

    Add web resources, and option menu

--- content.js
+++ content.js
@@ -1,16 +1,57 @@
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "highlightText") {
-    const highlightedText = window.getSelection().toString();
+    chrome.storage.sync
+      // get user's selected color
+      .get("option2")
+      .then(async (store) => {
+        const defaultColor = await getDefaultColor();
+        const color = store.option2 || defaultColor;
 
-    // Create a <span> element around the highlighted text with a yellow background
-    const span = document.createElement("span");
-    span.style.backgroundColor = "yellow";
-    span.textContent = highlightedText;
+        // Get selected text
+        const selection = window.getSelection();
+        if (selection && selection.rangeCount > 0) {
+          highlightSelection(selection, color);
+        }
+      })
 
-    // Replace the highlighted text with the <span> element
-    const range = window.getSelection().getRangeAt(0);
-    range.deleteContents();
-    range.insertNode(span);
+      .catch((e) => console.log(e));
   }
 });
+
+async function getDefaultColor() {
+  let constants;
+
+  try {
+    // chrome's silly way of fetching local resources
+    resourceUrl = await chrome.runtime.getURL("resources/constants.json");
+    const res = await fetch(resourceUrl);
+    constants = await res.json();
+  } catch (e) {
+    alert(
+      "Something went wrong when getting app constants. Defaulting to yellow"
+    );
+    console.log(e);
+  }
+
+  let defaultColor = "#FFFF00"; // absolute default
+  if (constants) {
+    defaultColor = constants.defaultColor;
+  }
+  return defaultColor;
+}
+
+function highlightSelection(selection, color) {
+  // Create a new range based on the selection
+  const highlightedText = selection.toString();
+
+  // Create a new span element for the highlight
+  const span = document.createElement("span");
+  span.style.backgroundColor = color;
+  span.textContent = highlightedText;
+
+  // Replace the highlighted text with the <span> element
+  const range = selection.getRangeAt(0);
+  range.deleteContents();
+  range.insertNode(span);
+}
--- options/options.js
+++ options/options.js
@@ -1,23 +1,62 @@
+// Remove this - just for adding color picker functionality in example
+const defaultColor = "#FFFF00";
+
+// delcare options
+const option1El = document.getElementById("option1");
+const option2El = document.getElementById("option2");
+
+const saveButton = document.getElementById("saveButton");
+const statusOutput = document.getElementById("status");
+
+var initialOption1Value = option1El.checked;
+var initialOption2Value = option2El.value;
+
+option1El.addEventListener("change", checkForChanges);
+option2El.addEventListener("change", checkForChanges);
+
+function checkForChanges() {
+  console.log({ initialOption1Value, initialOption2Value });
+  if (
+    option1El.checked !== initialOption1Value ||
+    option2El.value !== initialOption2Value
+  ) {
+    saveButton.disabled = false; // Enable the save button
+  } else {
+    saveButton.disabled = true; // Disable the save button
+  }
+}
+
+function resetChangeState() {
+  initialOption1Value = option1El.checked;
+  initialOption2Value = option2El.value;
+}
+
 // Load saved options or set default values
 document.addEventListener("DOMContentLoaded", function () {
   chrome.storage.sync.get(["option1", "option2"], function (result) {
-    document.getElementById("option1").checked = result.option1 || false;
-    document.getElementById("option2").value = result.option2 || "";
+    // add deafult vals after ||
+    option1El.checked = result.option1 || false;
+    option2El.value = result.option2 || defaultColor;
+
+    // disable save button unless options are changed
+    resetChangeState();
   });
 });
 
 // Save options when the "Save" button is clicked
-document.getElementById("saveButton").addEventListener("click", function () {
-  var option1 = document.getElementById("option1").checked;
-  var option2 = document.getElementById("option2").value;
+saveButton.addEventListener("click", function () {
+  var option1 = option1El.checked;
+  var option2 = option2El.value;
 
   // Save the options to Chrome storage
-  chrome.storage.sync.set({ option1: option1, option2: option2 }, function () {
+  chrome.storage.sync.set({ option1, option2 }, function (e) {
+    console.log(e);
     // Notify the user that the options have been saved
-    var status = document.getElementById("status");
-    status.textContent = "Options saved.";
+    statusOutput.textContent = "Options saved.";
+    resetChangeState();
+
     setTimeout(function () {
-      status.textContent = "";
+      statusOutput.textContent = "";
     }, 2000);
   });
 });
